97378c825025e0eb0fa83afcca5cb43904f02c2b
SashimiDaBest <soleilpham@gmail.com>
2024-10-31 22:20:54 -0400

    add changes from DMcTume

--- src/ChatInterface.java
+++ src/ChatInterface.java
@@ -1,5 +1,9 @@
+import java.util.*;
+
 public interface ChatInterface {
     public void addMessage (Message message);
     public void deleteMessage (Message message);
 
+    public String getChatID();
+    public ArrayList<String> getMessages();
 }
