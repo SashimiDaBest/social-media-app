891c36a0e825e380230444c253d77279abdf954c
SashimiDaBest <soleilpham@gmail.com>
2024-11-02 13:45:06 -0400

    update doc and revise code files

--- src/InvalidFileFormatException.java
+++ src/InvalidFileFormatException.java
@@ -1,3 +1,15 @@
+/**
+ * Social Media App - InvalidFileFormatException
+ *
+ * Subclass of exception that throws error when file format is not as expected
+ *
+ * Status: Complete
+ *
+ * @author connor pugliese
+ *
+ * @version 11/01/2024
+ *
+ */
 public class InvalidFileFormatException extends Exception {
     public InvalidFileFormatException(String message) {
         super(message);
