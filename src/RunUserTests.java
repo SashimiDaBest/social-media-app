ff3edaee5fc923d926a44e28c21c43aabbff4e0a
DMcTume <dmctume@purdue.edu>
2024-11-06 21:14:24 -0500

    Merged UserTest and RunUserTests and renamed the file

--- src/RunUserTests.java
+++ src/RunUserTests.java
@@ -1,3 +1,4 @@
+
 import org.junit.Test;
 
 import java.io.File;
@@ -7,7 +8,7 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 /**
- * Run User Tests Class
+ * User Tests
  * <p>
  * Tests some of the methods and constructors within the User class.
  * <p>
@@ -51,30 +52,39 @@ public class RunUserTests {
 
         testUsers[0].setProfilePic("test/path/pic.jpeg");
 
-        assertEquals("User does not properly get or assign userIDs.", "U_0000", testUsers[0].getUserID());
-        assertEquals("User does not properly get or assign usernames.", "userNumber0", testUsers[0].getUsername());
-        assertEquals("User does not properly get or assign passwords.", "userPassword0", testUsers[0].getPassWord());
-        assertEquals("User does not properly get or assign profile pic paths.", "test/path/pic.jpeg", testUsers[0].getProfilePic());
-        assertEquals("User does not properly get or assign account type.", 0, testUsers[0].getAccountType());
+        assertEquals("User does not properly get or " +
+                "assign userIDs.", "U_0000", testUsers[0].getUserID());
+        assertEquals("User does not properly get or assign usernames.",
+                "userNumber0", testUsers[0].getUsername());
+        assertEquals("User does not properly get or assign passwords.",
+                "userPassword0", testUsers[0].getPassWord());
+        assertEquals("User does not properly get or assign profile pic paths.",
+                "test/path/pic.jpeg", testUsers[0].getProfilePic());
+        assertEquals("User does not properly get or assign account type.",
+                0, testUsers[0].getAccountType());
 
         ArrayList<String> expectedFollowerList = new ArrayList<>();
         expectedFollowerList.add("U_0001");
         expectedFollowerList.add("U_0002");
-        assertEquals("User does not properly get or assign list of followers.", expectedFollowerList, testUsers[0].getFollowerList());
+        assertEquals("User does not properly get or assign list of followers.",
+                expectedFollowerList, testUsers[0].getFollowerList());
 
         ArrayList<String> expectedFollowingList = new ArrayList<>();
         expectedFollowingList.add("U_0000");
-        assertEquals("User does not properly get or assign list of following users.", expectedFollowingList, testUsers[1].getFollowingList());
+        assertEquals("User does not properly get or assign list of following users.",
+                expectedFollowingList, testUsers[1].getFollowingList());
 
         ArrayList<String> expectedBlockList = new ArrayList<>();
         expectedBlockList.add("U_0003");
         expectedBlockList.add("U_0004");
-        assertEquals("User does not properly get or assign list of blocked users.", expectedBlockList, testUsers[0].getBlockedList());
+        assertEquals("User does not properly get or assign list of blocked users.",
+                expectedBlockList, testUsers[0].getBlockedList());
 
         ArrayList<String> expectedChatList = new ArrayList<>();
         expectedChatList.add("C_0000");
         expectedChatList.add("C_0001");
-        assertEquals("User does not properly get or assign list of chats.", expectedChatList, testUsers[0].getChatIDList());
+        assertEquals("User does not properly get or assign list of chats.",
+                expectedChatList, testUsers[0].getChatIDList());
 
         testUsers[0].deleteFollower(testUsers[1].getUserID());
         testUsers[1].deleteFollowing(testUsers[0].getUserID());
@@ -83,7 +93,8 @@ public class RunUserTests {
         expectedFollowingList.remove(0);
 
         assertEquals("User does not properly remove followers.", expectedFollowerList, testUsers[0].getFollowerList());
-        assertEquals("User does not properly remove following users.", expectedFollowingList, testUsers[1].getFollowingList());
+        assertEquals("User does not properly remove following users.",
+                expectedFollowingList, testUsers[1].getFollowingList());
 
         for (int i = 0; i < 10; i++) {
             File f = new File("U_000" + i + ".txt");
@@ -104,8 +115,10 @@ public class RunUserTests {
         assertFalse("findUser() returns true when searching for a user that does not exist.",
                 testUsers[8].findUser("U_1000"));
 
-        assertTrue("userNameValidation() returns true on a free username.", testUsers[8].userNameValidation("free"));
-        assertFalse("userNameValidation() returns false on a taken username.", testUsers[8].userNameValidation("userNumber0"));
+        assertTrue("userNameValidation() returns true" +
+                " on a free username.", testUsers[8].userNameValidation("free"));
+        assertFalse("userNameValidation() returns" +
+                " false on a taken username.", testUsers[8].userNameValidation("userNumber0"));
 
         // Clear the UserIDList and ChatIDList files
         try (FileWriter fWriter = new FileWriter(f, false);
