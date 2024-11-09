09539debba14feaf475a88b8766275b98c1eeeb7
SashimiDaBest <soleilpham@gmail.com>
2024-11-09 16:14:15 -0500

    lol

--- src/SimpleServer.java
+++ src/SimpleServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -45,14 +47,21 @@ public class SimpleServer {
      * @throws IOException if an I/O error occurs while waiting for a connection
      */
     public void start() throws IOException {
-        System.out.println("Server listening on port " + serverSocket.getLocalPort());
-        while (true) {
-            try {
-                Socket socket = serverSocket.accept();
-//                executorService.submit(new ClientHandler(clientSocket));
-            } catch (IOException e) {
-                System.out.println("Error accepting connection" + e.getMessage());
-            }
+        try {
+            Socket socket = serverSocket.accept();
+            action();
+//          executorService.submit(new ClientHandler(clientSocket));
+        } catch (IOException e) {
+            System.out.println("Error accepting connection" + e.getMessage());
+        }
+    }
+
+    public void action() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = in.readLine();
+            System.out.println(line);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -80,6 +89,7 @@ public class SimpleServer {
         try {
             SimpleServer server = new SimpleServer(12);
             server.start();
+
         } catch (IOException e) {
             System.err.println("Server error: " + e.getMessage());
         }
